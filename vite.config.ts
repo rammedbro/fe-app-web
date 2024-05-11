73f34e738ee32c250cf7a3de168775be009e489e
Ramil Sharipov <rammedbro@yandex.ru>
2024-05-11 23:24:55 +0500

    build(FAW-26): Make css modules' classes consisting only of hash in prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type CSSModulesOptions } from 'vite';
+import { createHash } from 'node:crypto';
 import vue from '@vitejs/plugin-vue';
 import pkg from './package.json';
 
-export default defineConfig({
+const generateScopedName: CSSModulesOptions['generateScopedName'] = (name, filename) => {
+  const hash = createHash('md5').update(filename).digest('hex').slice(0, 5);
+  const file = filename.slice(filename.lastIndexOf('/') + 1, filename.indexOf('.'));
+
+  return `${ file }__${ name }__${ hash }`;
+};
+
+export default defineConfig((config) => ({
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
@@ -15,11 +23,11 @@ export default defineConfig({
     },
     modules: {
       scopeBehaviour: 'local',
-      generateScopedName: '[local]__[hash:base64:5]',
+      generateScopedName: (config.mode === 'production') ? '[hash:base64:5]' : generateScopedName,
       localsConvention: 'camelCaseOnly',
     },
   },
   plugins: [
     vue(),
   ],
-});
+}));
