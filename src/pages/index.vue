434a8135cf3c83c22c7fa7345fb292ba8b1d164c
Ramil Sharipov <rammedbro@yandex.ru>
2024-11-09 20:07:02 +0000

    feat(FAW-32): Add css framework

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,9 @@
 <template>
   <section>
-    <h1 :class="$style.title">Scaffolding frontend applications</h1>
+    <h1 class="text-4xl">Scaffolding frontend applications</h1>
 
     <p>
       Welcome to my awesome application scaffolded with fe-app!
     </p>
   </section>
 </template>
-
-<style module>
-.title {
-  font-size: 32px;
-  word-spacing: 100vw;
-}
-</style>
