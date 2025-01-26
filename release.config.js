f944b07d1253c69d7b765669884a91a17f9fe4dc
Ramil Sharipov <rammedbro@yandex.ru>
2025-01-27 00:41:57 +0300

    chore(FAW-44): Add release notes to releases

--- release.config.js
+++ release.config.js
@@ -7,6 +7,7 @@ export default {
   branches: ['master', 'next'],
   plugins: [
     '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     [
       '@semantic-release/gitlab',
